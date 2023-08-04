139f7ee61e3ad5d6adfe144512a23c0df8743cf7
Vamsi1656 <vamsireddy1717@gmail.com>
2023-08-04 15:51:34 +0530

    Java

---
+++ Variables/Demo.java
@@ -0,0 +1,18 @@
+class Demo{
+
+            int a = 10;
+            static String b="Nani";
+
+    public static void main(String args[]){
+             int c=100;
+             
+        Demo d1=new Demo();
+        Demo d2=new Demo();
+       
+        System.out.println(d1.a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+}
