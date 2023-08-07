e00dd7909ae276955f903502de4cc068245e8253
Vamsi1656 <vamsireddy1717@gmail.com>
2023-08-07 15:45:36 +0530

    Java

---
+++ ControlFlowSt/ConditionalSt/Ternary.java
@@ -0,0 +1,14 @@
+class Ternary{
+
+
+    public static void main(String[] args){
+
+        int a=10;
+       String msg = a>=100 && a<=999 ? "Given number is Three digit nuumber" : "Given number is not three digit number";
+       System.out.println(msg);
+    }
+}
+
+// The alternate of if-else statement is by using Ternary Operator.
+// The sysntax of Ternary Operator is:
+// condition st ? true : false
