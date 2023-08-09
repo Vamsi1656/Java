8f9fbd8638b0e3b862b963f4d15c3e7afad07886
Vamsi1656 <vamsireddy1717@gmail.com>
2023-08-09 16:14:08 +0530

    JAVA

---
+++ Patterns/TestTwo.java
@@ -0,0 +1,18 @@
+class TestTwo{
+
+
+    public static void main(String[] args){
+
+        //  int i,j;
+
+        for(int i=0;i<=6;i++){
+            for (int j=6-i; j>=1; j--){  
+                    System.out.print(" ");   
+                }   
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
